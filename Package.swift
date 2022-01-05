364fdd58e77853f51452175a500e912407a067e8
Lance Mao <maolongdong@gmail.com>
2022-01-05 08:49:32 +0800

    modify package path

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Guard",
-            url: "https://github.com/Authing/guard_ios/releases/download/1.0.1/Guard.xcframework.zip",
+            url: "https://github.com/Authing/guard-ios/releases/download/1.0.1/Guard.xcframework.zip",
             checksum: "3ec6cf14e51b8f66e3e3ce116417266c1e84ee8bc0e8e43f16a328964a457fca"
         )
     ]
