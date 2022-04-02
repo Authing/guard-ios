21911ede86f6b72bcd116d082cc3a8494e117202
jnMars <563438383@qq.com>
2022-04-02 14:46:17 +0800

    Fix - submit CountryCodeSearchBar

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Guard",
-            url: "https://github.com/Authing/guard-ios/releases/download/1.0.8/Guard.xcframework.zip",
-            checksum: "bd391d3c98573ea3a45f0c60dc0e8e3d9037b37670aecce7970176473a5a2988"
+            url: "https://github.com/Authing/guard-ios/releases/download/1.0.9/Guard.xcframework.zip",
+            checksum: "fb63a96199907c9348ec76fd2508bff0a56428abf9e6c0f90821eff25203fc16"
         )
     ]
 )
