e908b1b2e385d557a1f6980895d59df9973d72d3
jnMars <563438383@qq.com>
2022-04-20 15:58:07 +0800

    Fix - checksum

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .binaryTarget(
             name: "Guard",
             url: "https://github.com/Authing/guard-ios/releases/download/1.1.1/Guard.xcframework.zip",
-            checksum: "cc60edf475f80e92575269b26a5f03c921fad78f6eac010abe2cdfabf180a716"
+            checksum: "85922e1c6e2e83f2b8ded0bd022e05524aed9eee3d243d754ca593002a5959bc"
         )
     ]
 )
