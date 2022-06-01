d53eada179585eed8fcd39667a653255c5828c47
Lance Mao <maolongdong@gmail.com>
2022-06-01 16:23:44 +0800

    report device model and OS version

--- Guard/Guard/Analyze/SDKUsageTask.swift
+++ Guard/Guard/Analyze/SDKUsageTask.swift
@@ -9,15 +9,24 @@ import Foundation
 
 class SDKUsageTask {
     static func report() {
-        let uuid: String = Util.getDeviceID()
-        let url:URL! = URL(string:"https://developer-beta.authing.cn/stats/sdk-trace/?appid=" + Authing.getAppId()
-                           + "&sdk=ios&version=" + Const.SDK_VERSION
-                           + "&ip=" + uuid)
-        URLSession.shared.dataTask(with: url) { (objectData, response, error) in
-            guard error == nil else {
-                print("network error:\(error!.localizedDescription)")
-                return
+        DispatchQueue.global().async {
+            let uuid: String = Util.getDeviceID()
+            let deviceName = UIDevice.modelName
+            let systemVersion = UIDevice.current.systemVersion
+            if let escapedString = deviceName.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) {
+                let urlString = "https://developer-beta.authing.cn/stats/sdk-trace/?appid=" + Authing.getAppId()
+                + "&sdk=ios&version=" + Const.SDK_VERSION
+                + "&ip=\(uuid)"
+                + "&device=\(escapedString)"
+                + "&os-version=\(systemVersion)"
+                let url:URL! = URL(string: urlString)
+                URLSession.shared.dataTask(with: url) { (objectData, response, error) in
+                    guard error == nil else {
+                        ALog.w(SDKUsageTask.self, "network error:\(error!.localizedDescription)")
+                        return
+                    }
+                }.resume()
             }
-        }.resume()
+        }
     }
 }
