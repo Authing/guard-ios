f73976db58422a1888f72623f7c75072c787d5fa
Lance Mao <maolongdong@gmail.com>
2022-06-14 14:18:21 +0800

    minor trace fix

--- Guard/Guard/Analyze/SDKUsageTask.swift
+++ Guard/Guard/Analyze/SDKUsageTask.swift
@@ -5,7 +5,6 @@
 //  Created by Lance Mao on 2021/12/3.
 //
 
-import Foundation
 import UIKit
 
 class SDKUsageTask {
@@ -18,12 +17,12 @@ class SDKUsageTask {
                 let urlString = "https://developer-beta.authing.cn/stats/sdk-trace/?appid=" + Authing.getAppId()
                 + "&sdk=ios&version=" + Const.SDK_VERSION
                 + "&ip=\(uuid)"
+                + "&host=\(Authing.getHost())"
                 + "&device=\(escapedString)"
                 + "&os-version=\(systemVersion)"
                 let url:URL! = URL(string: urlString)
                 URLSession.shared.dataTask(with: url) { (objectData, response, error) in
                     guard error == nil else {
-                        ALog.w(SDKUsageTask.self, "network error:\(error!.localizedDescription)")
                         return
                     }
                 }.resume()
