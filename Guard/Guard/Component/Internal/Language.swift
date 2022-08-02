ef48ce88f25612bb0a38422bc20364d055db1078
jnMars <563438383@qq.com>
2022-08-02 18:15:36 +0800

    New - configuration international language

---
+++ Guard/Guard/Component/Internal/Language.swift
@@ -0,0 +1,54 @@
+//
+//  Language.swift
+//  Guard
+//
+//  Created by JnMars on 2022/8/2.
+//
+
+import Foundation
+
+enum Language: String, CaseIterable {
+    case English, Japanese, Chinese, ChineseT
+    
+    var code: String {
+        switch self {
+        case .English: return "en"
+        case .Japanese: return "ja"
+        case .Chinese: return "zh-Hans"
+        case .ChineseT: return "zh-Hant"
+        }
+    }
+
+    static var lang: Language {
+        set {
+            UserDefaults.standard.set(newValue.rawValue, forKey: "language")
+            UserDefaults.standard.synchronize()
+        }
+        get {
+            return Language(rawValue: UserDefaults.standard.string(forKey: "language") ?? "") ?? .English
+        }
+    }
+
+    static func setLanguage(_ language: String) {
+        var languageType: Language = .English
+        switch language {
+        case "zh-CN", "zh_CN":
+            languageType = .Chinese
+            break
+        case "en-US", "en_US":
+            languageType = .English
+            break
+        case "zh-TW", "zh_TW":
+            languageType = .ChineseT
+            break
+        case "ja-JP", "ja_JP":
+            languageType = .Japanese
+            break
+        default:
+            languageType = .Chinese
+            break
+        }
+        lang = languageType
+    }
+    
+}
--- Guard/Guard/Extension/String+Authing.swift
+++ Guard/Guard/Extension/String+Authing.swift
@@ -9,7 +9,8 @@ import Foundation
 
 extension String {
     var L: String {
-        let bundle = Bundle(identifier: "cn.authing.Guard")
-        return NSLocalizedString(self, bundle: bundle ?? Bundle.init(), comment: "")
+        let path = Bundle(identifier: "cn.authing.Guard")?.path(forResource: Language.lang.code, ofType: "lproj")
+        let bundle = Bundle(path: path ?? "") ?? Bundle()
+        return NSLocalizedString(self, tableName: nil, bundle: bundle, value: "", comment: "")
     }
 }
