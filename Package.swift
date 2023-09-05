ef7381734e3e6a75a4bc2bf2a46b2d6d94e2d545
jnMars <563438383@qq.com>
2023-09-05 18:10:21 +0800

    Version - 1.4.9

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Guard",
-            url: "https://github.com/Authing/guard-ios/releases/download/1.4.8/Guard.xcframework.zip",
-            checksum: "f6cef2a784cbd2a6791d30b3d9fd146be4f2e5cc610a768b6a7eaf54319fd3ab"
+            url: "https://github.com/Authing/guard-ios/releases/download/1.4.9/Guard.xcframework.zip",
+            checksum: "c8abe31dda7c8d9bde4c23cd91cbb7c01c919d9324fbc89d823567669dac634e"
         )
     ]
 )
